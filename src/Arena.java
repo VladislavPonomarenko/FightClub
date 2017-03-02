89c5917efbde9496e60c7fece0d8e7187324c168
VladislavPonomarenko <vladponomarenko8@gmail.com>
2017-03-02 21:58:05 +0200

    0203172157

--- src/Arena.java
+++ src/Arena.java
@@ -13,44 +13,49 @@ public class Arena {
 
         if (random.nextBoolean() == true) {
             System.out.println("GO fighter" + fighter1);
-            for (int i = 0; i < 10; i++) {
-                if (fighter2.getXp() != 0 || fighter2.getXp() > 0 || fighter1.getXp() != 0 || fighter1.getXp() > 0) {
-                    Fighter1(fighter1, fighter2);
-                    Fighter2(fighter1, fighter2);
-                }
-            }
 
-//
+            do{
+                Fighter1(fighter1, fighter2);
+                Fighter2(fighter1, fighter2);
+
+            }while (fighter2.getXp() != 0 && fighter2.getXp() > 0 && fighter1.getXp() != 0 && fighter1.getXp() > 0);
+            if (fighter2.getXp() == 0 || fighter2.getXp() < 0) {
+                fighter2.setXp(0);
+                System.out.println(fighter2.getName() + ": " + fighter2.getXp() + "xp");
+                System.out.println("Win " + fighter1.getName());
+            }else{
+                fighter1.setXp(0);
+                System.out.println(fighter1.getName() + ": " + fighter1.getXp() + "xp");
+                System.out.println("Win " + fighter2.getName());
+            }
 
         } else {
             System.out.println("GO fighter" + fighter2);
-            for (int i = 0; i < 10; i++) {
-                if (fighter2.getXp() != 0 || fighter2.getXp() > 0 || fighter1.getXp() != 0 || fighter1.getXp() > 0) {
-                    Fighter2(fighter1, fighter2);
-                    Fighter1(fighter1, fighter2);
+
+           do{
+
+            Fighter2(fighter1, fighter2);
+            Fighter1(fighter1, fighter2);
+        } while (fighter2.getXp() != 0 && fighter2.getXp() > 0 && fighter1.getXp() != 0 && fighter1.getXp() > 0);
+                if (fighter1.getXp() == 0 || fighter1.getXp() < 0) {
+                    fighter1.setXp(0);
+                    System.out.println(fighter1.getName() + ": " + fighter1.getXp() + "xp");
+                    System.out.println("Win " + fighter2.getName());
+                }else{
+                    fighter2.setXp(0);
+                    System.out.println(fighter2.getName() + ": " + fighter2.getXp() + "xp");
+                    System.out.println("Win " + fighter1.getName());
                 }
-            }
-
         }
-
-
     }
 
     public void Fighter1(Fighter fighter1, Fighter fighter2) {
         Random random = new Random();
         int cnt = fighter2.getXp();
-        if (fighter2.getXp() == 0 || fighter2.getXp() < 0) {
-            System.out.println("Win " + fighter1.getName());
-        } else {
-
+        if (cnt != 0 && cnt > 0) {
             fighter2.setXp(fighter2.getXp() - fighter1.getForse());
-            if (fighter2.getXp() == 0 || fighter2.getXp() < 0) {
-                fighter2.setXp(0);
-                System.out.println(fighter2.getName() + ": " + fighter2.getXp() + "xp");
-            } else
-                System.out.println(fighter2.getName() + ": " + fighter2.getXp() + "xp" + "(-" + (cnt - fighter2.getXp()) + ")");
+            System.out.println(fighter2.getName() + ": " + fighter2.getXp() + "xp" + "(-" + (cnt - fighter2.getXp()) + ")");
             fighter1.setForse(random.nextInt(30) + 1);
-
         }
         cnt = fighter2.getXp();
 
@@ -59,15 +64,9 @@ public class Arena {
     public void Fighter2(Fighter fighter1, Fighter fighter2) {
         Random random = new Random();
         int cnt = fighter1.getXp();
-        if (fighter1.getXp() == 0 || fighter1.getXp() < 0) {
-            System.out.println("Win " + fighter2.getName());
-        } else {
+        if (cnt != 0 && cnt > 0) {
             fighter1.setXp(fighter1.getXp() - fighter2.getForse());
-            if (fighter1.getXp() == 0 || fighter1.getXp() < 0) {
-                fighter1.setXp(0);
-                System.out.println(fighter1.getName() + ": " + fighter1.getXp() + "xp");
-            } else
-                System.out.println(fighter1.getName() + ": " + fighter1.getXp() + "xp" + "(-" + (cnt - fighter1.getXp()) + ")");
+            System.out.println(fighter1.getName() + ": " + fighter1.getXp() + "xp" + "(-" + (cnt - fighter1.getXp()) + ")");
             fighter2.setForse(random.nextInt(29) + 2);
         }
         cnt = fighter1.getXp();
