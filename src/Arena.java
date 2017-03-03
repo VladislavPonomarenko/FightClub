92e6a210a8367e471494db0da52065fd95ea8244
VladislavPonomarenko <vladponomarenko8@gmail.com>
2017-03-03 09:25:18 +0200

    0303170925

--- src/Arena.java
+++ src/Arena.java
@@ -53,13 +53,19 @@ public class Arena {
 
     public void Fighter1(Fighter fighter1, Fighter fighter2) {
         Random random = new Random();
-        double krit=random.nextDouble();
         int differenceXp = fighter2.getXp();
-        if (differenceXp != 0 && differenceXp > 0) {
 
-            if(krit<(double)1/3){
+        int crit =random.nextInt(10);
+        fighter2.setIntuition(random.nextInt(10));
+        fighter2.setDexterity(random.nextInt(10));
+        if (differenceXp != 0 && differenceXp > 0) {
+          //  System.out.println(crit+" : "+fighter2.getIntuition());
+            if(crit == fighter2.getDexterity()){
+                System.out.println(fighter2.getName()+": "+fighter2.getXp()+"xp"+"\033[34m (deviation)"+"\033[38m");
+            }else
+            if(crit == fighter2.getIntuition()){
                 fighter2.setXp(fighter2.getXp() - (fighter1.getForse()*2));
-                System.out.println(fighter2.getName() + ": " + fighter2.getXp() + "xp" + "(-" + (differenceXp - fighter2.getXp()) + " krit)");
+                System.out.println(fighter2.getName() + ": " + fighter2.getXp() + "xp" + "(-" + (differenceXp - fighter2.getXp()) + "\033[31m crit)"+"\033[38m");
             }else {
                 fighter2.setXp(fighter2.getXp() - fighter1.getForse());
                 System.out.println(fighter2.getName() + ": " + fighter2.getXp() + "xp" + "(-" + (differenceXp - fighter2.getXp()) + ")");
@@ -73,11 +79,17 @@ public class Arena {
     public void Fighter2(Fighter fighter1, Fighter fighter2) {
         Random random = new Random();
         int differenceXp = fighter1.getXp();
-        double krit=random.nextDouble();
+
+        int crit =random.nextInt(10);
+        fighter1.setIntuition(random.nextInt(10));
+        fighter1.setDexterity(random.nextInt(10));
         if (differenceXp != 0 && differenceXp > 0) {
-            if(krit<(double)1/3){
+            if(crit == fighter1.getDexterity()){
+                System.out.println(fighter1.getName()+": "+fighter1.getXp()+"xp"+"\033[34m (deviation)"+"\033[38m");
+            }else
+            if(crit == fighter1.getIntuition()){
                 fighter1.setXp(fighter1.getXp() - (fighter2.getForse()*2));
-                System.out.println(fighter1.getName() + ": " + fighter1.getXp() + "xp" + "(-" + (differenceXp - fighter1.getXp()) + " krit)");
+                System.out.println(fighter1.getName() + ": " + fighter1.getXp() + "xp" + "(-" + (differenceXp - fighter1.getXp()) + "\033[31m crit)"+"\033[38m");
             }else {
             fighter1.setXp(fighter1.getXp() - fighter2.getForse());
             System.out.println(fighter1.getName() + ": " + fighter1.getXp() + "xp" + "(-" + (differenceXp - fighter1.getXp()) + ")");
